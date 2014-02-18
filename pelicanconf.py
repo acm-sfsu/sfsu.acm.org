4bbe3d3a97c6ee3fe815c3c536fbfdf849508a11
Tommy <tom@mechanicalape.com>
2014-02-18 15:48:31 -0800

    a couple links

--- pelicanconf.py
+++ pelicanconf.py
@@ -1,70 +1,31 @@
-# -*- coding: utf-8 -*-
+#!/usr/bin/env python
+# -*- coding: utf-8 -*- #
 from __future__ import unicode_literals
 
-AUTHOR = 'Alexis Métaireau'
-SITENAME = "Alexis' log"
-SITEURL = 'http://blog.notmyidea.org'
-TIMEZONE = "Europe/Paris"
+AUTHOR = u'Tom Spalding'
+SITENAME = u'ACM at SFSU'
+SITEURL = ''
 
-# can be useful in development, but set to False when you're ready to publish
+TIMEZONE = 'America/Los_Angeles'
+
+DEFAULT_LANG = u'en'
+
+# Feed generation is usually not desired when developing
+FEED_ALL_ATOM = None
+CATEGORY_FEED_ATOM = None
+TRANSLATION_FEED_ATOM = None
+
+# Blogroll
+LINKS =  (('ACM-SFSU Github', 'https://github.com/acm-sfsu'),
+          ('ACM International', 'http://acm.org'),
+          ('Computer Science Department', 'http://cs.sfsu.edu'),)
+
+# Social widget
+SOCIAL = (('Github', 'https://github.com/acm-sfsu'),
+          ('IRC','http://webchat.freenode.net/?channels=acm-sfsu'),
+          ('Twitter', '#'),)
+
+DEFAULT_PAGINATION = 10
+
+# Uncomment following line if you want document-relative URLs when developing
 RELATIVE_URLS = True
-
-GITHUB_URL = 'http://github.com/ametaireau/'
-DISQUS_SITENAME = "blog-notmyidea"
-PDF_GENERATOR = False
-REVERSE_CATEGORY_ORDER = True
-LOCALE = "C"
-DEFAULT_PAGINATION = 4
-DEFAULT_DATE = (2012, 3, 2, 14, 1, 1)
-
-FEED_ALL_RSS = 'feeds/all.rss.xml'
-CATEGORY_FEED_RSS = 'feeds/%s.rss.xml'
-
-LINKS = (('Biologeek', 'http://biologeek.org'),
-         ('Filyb', "http://filyb.info/"),
-         ('Libert-fr', "http://www.libert-fr.com"),
-         ('N1k0', "http://prendreuncafe.com/blog/"),
-         ('Tarek Ziadé', "http://ziade.org/blog"),
-         ('Zubin Mithra', "http://zubin71.wordpress.com/"),)
-
-SOCIAL = (('twitter', 'http://twitter.com/ametaireau'),
-          ('lastfm', 'http://lastfm.com/user/akounet'),
-          ('github', 'http://github.com/ametaireau'),)
-
-# global metadata to all the contents
-DEFAULT_METADATA = (('yeah', 'it is'),)
-
-# path-specific metadata
-EXTRA_PATH_METADATA = {
-    'extra/robots.txt': {'path': 'robots.txt'},
-    }
-
-# static paths will be copied without parsing their contents
-STATIC_PATHS = [
-    'pictures',
-    'extra/robots.txt',
-    ]
-
-# custom page generated with a jinja2 template
-TEMPLATE_PAGES = {'pages/jinja2_template.html': 'jinja2_template.html'}
-
-# code blocks with line numbers
-PYGMENTS_RST_OPTIONS = {'linenos': 'table'}
-
-# foobar will not be used, because it's not in caps. All configuration keys
-# have to be in caps
-foobar = "barbaz"
-
-# https://gist.github.com/josefjezek/6053301
-THEME = 'themes/bootstrap2'
-OUTPUT_PATH = 'output'
-PATH = 'content'
-
-# http://docs.getpelican.com/en/latest/settings.html#url-settings
-ARTICLE_URL = 'posts/{date:%Y}/{date:%m}/{slug}/'
-ARTICLE_SAVE_AS = 'posts/{date:%Y}/{date:%m}/{slug}/index.html'
-
-# Custom Home page
-DIRECT_TEMPLATES = (('index', 'blog', 'tags', 'categories', 'archives'))
-PAGINATED_DIRECT_TEMPLATES = (('blog',))
-TEMPLATE_PAGES = {'home.html': 'index.html',}
